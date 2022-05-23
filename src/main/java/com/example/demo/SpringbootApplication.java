17b34d6d20298f7309dfd3894b685d2a2d7d7794
rompeteshos <rompeteshos@hotmail.com>
2022-05-23 16:44:36 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/SpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import com.example.demo.entities.Customer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+
+
+		Customer asd = new Customer();
+		asd.getFirstname();
+
+
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
