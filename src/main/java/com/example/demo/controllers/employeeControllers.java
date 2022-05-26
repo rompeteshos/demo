7dd1761cccff66f2ff04d3732360cb6130167cdc
rompeteshos <rompeteshos@hotmail.com>
2022-05-26 13:21:09 +0200

    servidos agregadas listas customer employee y supplier min:3.38

---
+++ src/main/java/com/example/demo/controllers/employeeControllers.java
@@ -0,0 +1,37 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.Employee;
+import com.example.demo.services.ICostumerService;
+import com.example.demo.services.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class employeeControllers {
+
+    @Autowired
+    private IEmployeeService service;
+
+    @GetMapping("/api/employees")
+    public List<Employee> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/api/employees/{id}")
+    public Employee getByid(@PathVariable String id) {
+        return service.getById(Long.parseLong(id));
+    }
+
+    @DeleteMapping("/api/Employees/{id}")
+    public void remove(@PathVariable String id) {
+        service.remove(Long.parseLong(id));
+    }
+
+    @PostMapping("/api/employees")
+    public void save(@RequestBody Employee employee)    {
+        service.save(employee);
+    }
+
+}
